04a233723aa709c3635f19edf13558a8c97447a3
Stephon Smith <smithwrestling89@gmail.com>
2020-10-17 00:01:33 -0400

    added contact page with form validations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import Nav from './components/Nav';
+import ContactForm from './components/Contact';
 import './App.css';
 
 function App() {
   return (
-    <header>
+    <main>
       <Nav />
-    </header>
+      <ContactForm />
+    </main>
   );
 }
 
