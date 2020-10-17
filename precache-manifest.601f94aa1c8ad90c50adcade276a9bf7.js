cca273c35b63e29f6444335abec7076402638e3e
Stephon Smith <smithwrestling89@gmail.com>
2020-10-17 14:44:56 -0400

    Updates

---
+++ precache-manifest.601f94aa1c8ad90c50adcade276a9bf7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c33bc69aa8f289e50a051639d21790",
+    "url": "/REACT-portfolio/index.html"
+  },
+  {
+    "revision": "72f1ca286b02dd84b614",
+    "url": "/REACT-portfolio/static/css/2.72381884.chunk.css"
+  },
+  {
+    "revision": "90f9ac3b48f8ca10b0c6",
+    "url": "/REACT-portfolio/static/css/main.400b2460.chunk.css"
+  },
+  {
+    "revision": "72f1ca286b02dd84b614",
+    "url": "/REACT-portfolio/static/js/2.9524d151.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/REACT-portfolio/static/js/2.9524d151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f9ac3b48f8ca10b0c6",
+    "url": "/REACT-portfolio/static/js/main.c130193c.chunk.js"
+  },
+  {
+    "revision": "a65105b066a40284b1c4",
+    "url": "/REACT-portfolio/static/js/runtime-main.ef72c244.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/REACT-portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/REACT-portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/REACT-portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/REACT-portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/REACT-portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/REACT-portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/REACT-portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/REACT-portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/REACT-portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/REACT-portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/REACT-portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/REACT-portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/REACT-portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/REACT-portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/REACT-portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/REACT-portfolio/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.9502b45a702ae823c5ef67706c9751fa.js"
+  "/REACT-portfolio/precache-manifest.601f94aa1c8ad90c50adcade276a9bf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/REACT-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
