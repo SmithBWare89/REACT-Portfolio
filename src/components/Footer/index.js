6f4097e9f55a928fcb24b39352e1d14da2135607
Stephon Smith <smithwrestling89@gmail.com>
2021-08-23 01:44:21 -0400

    updated image links footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,17 +7,17 @@ export default function Footer() {
             <Container id="footer-container">
                 <Grid columns={3}>
                     <Grid.Column>
-                        <a href="https://www.github.com/SmithBWare89" name='gitHubRepo'>
+                        <a name="gitHubRepo" href="https://www.github.com/SmithBWare89">
                             <i className="fab fa-github fa-3x" id="github"></i>
                         </a>
                     </Grid.Column>
                     <Grid.Column>
-                        <a href="https://stackoverflow.com/users/13443220/smithbware89" name='stackOverflow'>
+                        <a name="stackOverflow" href="https://stackoverflow.com/users/13443220/smithbware89">
                             <i className="fab fa-stack-overflow fa-3x" id="stack-overflow"></i>
                         </a>
                     </Grid.Column>
                     <Grid.Column>
-                        <a href="https://www.linkedin.com/in/stephonsmith89/" name='linkedIn'>
+                        <a name="linkedIn" href="https://www.linkedin.com/in/stephonsmith89/">
                             <i className="fab fa-linkedin fa-3x" id="linked-in"></i>
                         </a>
                     </Grid.Column>
