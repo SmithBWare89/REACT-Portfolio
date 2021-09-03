02c4a50e4b9517e44cffd74be73b6a4048fc33e9
Stephon Smith <smithwrestling89@gmail.com>
2021-09-03 07:30:44 -0400

    attempted styling skill images

--- src/components/SkillImages/index.js
+++ src/components/SkillImages/index.js
@@ -16,16 +16,16 @@ import sql from '../../assets/images/skills/sql.png';
 export default function SkillImages() {
     return (
         <div className="skills">
-            <Image className="skill-images" size="small" name="html5" src={html5} centered={true} alt="HTML 5 icon"/>
-            <Image className="skill-images" size="small" name="css3" src={css3} centered={true} alt="CSS 3 icon"/>
-            <Image className="skill-images" size="small" name="javascript" src={javascript} centered={true} alt="JavaScript icon"/>
-            <Image className="skill-images" size="small" name="node" src={node} centered={true} alt="Node icon"/>
-            <Image className="skill-images" size="small" name="react" src={react} centered={true} alt="React icon"/>
-            <Image className="skill-images" size="small" name="mongodb" src={mongodb} centered={true} alt="MongoDB icon"/>
-            <Image className="skill-images" size="small" name="heroku" src={heroku} centered={true} alt="Heroku icon"/>
-            <Image className="skill-images" size="small" name="sql" src={sql} centered={true} alt="SQL icon"/>
-            <Image className="skill-images" size="small" name="jquery" src={jquery} centered={true} alt="Jquery icon"/>
-            <Image className="skill-images" size="small" name="bootstrap" src={bootstrap} centered={true} alt="Bootstrap icon"/>
+            <Image className="skill-images" size="small" name="html5" src={html5} centered={true} rounded={true} alt="HTML 5 icon"/>
+            <Image className="skill-images" size="small" name="css3" src={css3} centered={true} rounded={true} alt="CSS 3 icon"/>
+            <Image className="skill-images" size="small" name="javascript" src={javascript} centered={true} rounded={true} alt="JavaScript icon"/>
+            <Image className="skill-images" size="small" name="node" src={node} centered={true} rounded={true} alt="Node icon"/>
+            <Image className="skill-images" size="small" name="react" src={react} centered={true} rounded={true} alt="React icon"/>
+            <Image className="skill-images" size="small" name="mongodb" src={mongodb} centered={true} rounded={true} alt="MongoDB icon"/>
+            <Image className="skill-images" size="small" name="heroku" src={heroku} centered={true} rounded={true} alt="Heroku icon"/>
+            <Image className="skill-images" size="small" name="sql" src={sql} centered={true} rounded={true} alt="SQL icon"/>
+            <Image className="skill-images" size="small" name="jquery" src={jquery} centered={true} rounded={true} alt="Jquery icon"/>
+            <Image className="skill-images" size="small" name="bootstrap" src={bootstrap} centered={true} rounded={true} alt="Bootstrap icon"/>
         </div>
     )
 }
